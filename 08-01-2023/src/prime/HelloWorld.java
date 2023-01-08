ebbf4e486930a91038dda5c4a1d95bc35a967079
Mohit Krishna <mohitkrishna.s.s12@gmail.com>
2023-01-08 08:14:13 +0530

    initial commit

---
+++ 08-01-2023/src/prime/HelloWorld.java
@@ -0,0 +1,12 @@
+package prime;
+import prime.PrimeOrNot;
+
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        //PrimeOrNot n= new PrimeOrNot();
+        System.out.println(PrimeOrNot.name);
+    }
+
+}
---
+++ 08-01-2023/src/prime/PrimeOrNot.java
@@ -0,0 +1,7 @@
+package prime;
+public class PrimeOrNot {
+    protected static String name="nishanth";
+    public static void main(String[] args) {
+        System.out.println("mohit");
+    }
+}
